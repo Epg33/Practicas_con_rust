8e345b6e2509b089e59472591db0edeeade1dc6e
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-11-22 14:08:48 -0500

    operations

--- cli-calc/src/main.rs
+++ cli-calc/src/main.rs
@@ -1,3 +1,21 @@
+use std::result;
+
+mod operations;
+
 fn main() {
     println!("Hello, world!");
+    let operation=options("+");
+    println!("{operation}")
+}
+
+fn options(opt: &str) -> i32 {
+    let mut result: i32 = 0;
+    match opt {
+        "+" => result = operations::adding(10, 15),
+        "-" => result = operations::substrackting(15, 20),
+        "*" => result = operations::multipliying(10, 10),
+        "/" => result = operations::dividing(10, 10),
+        &_ => println!("insert a valid operation"),
+    };
+    return result;
 }
---
+++ cli-calc/src/operations.rs
@@ -0,0 +1,19 @@
+pub fn adding(num1:i32, num2:i32)->i32 {
+  let result:i32 = num1+num2;
+  return result
+}
+
+pub fn substrackting(num1:i32, num2:i32)->i32 {
+  let result:i32 = num1-num2;
+  return result
+}
+
+pub fn multipliying(num1:i32, num2:i32)->i32 {
+  let result:i32 = num1*num2;
+  return result
+}
+
+pub fn dividing(num1:i32, num2:i32)->i32 {
+  let result:i32 = num1/num2;
+  return result
+}
