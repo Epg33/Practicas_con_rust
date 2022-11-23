3cbcc2a12d0442596987f9d4923364e1aa792c6f
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-11-23 13:56:55 -0500

    xd

--- cli-calc/src/main.rs
+++ cli-calc/src/main.rs
@@ -4,12 +4,13 @@ mod operations;
 fn main() {
     println!("Please input the numbers you want");
     println!("first");
-    let mut inp_num1:i32 = 0;
     let mut str1:String = String::new();
     io::stdin()
         .read_line(&mut str1)
         .expect("insert a valid number");
-    inp_num1= str1.parse::<i32>().unwrap();
+    let inp_num1 = str1.parse::<i32>().unwrap();
+    // inp_num1 = FromStr::from_str(str1).unwrap();
+    // inp_num1= str1.parse::<i32>().unwrap();
 
     println!("second");
     let mut inp_num2:i32 = 0;
