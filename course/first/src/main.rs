9c1a5843ea8941aa97c5b8d54a8f23d7a1f3f0c0
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-11-24 15:20:07 -0500

    szs

---
+++ course/first/src/main.rs
@@ -0,0 +1,11 @@
+#![allow(unused)]
+
+use std::io;
+use rand::Rng;
+use std::io::{write, BufReader, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Hello, world!");
+}
