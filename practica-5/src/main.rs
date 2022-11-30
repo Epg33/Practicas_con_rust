2e7c23edf08c04cf52dddcf6469c1e4b6cafa3b7
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-11-30 15:24:22 -0500

    struct

--- practica-5/src/main.rs
+++ practica-5/src/main.rs
@@ -1,3 +1,31 @@
+struct User {
+    active: bool,
+    user_name: String,
+    email: String,
+    id: u64
+}
+struct Rgb(i32, i32, i32);
+struct Point(i32, i32, i32);
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = User{
+        active: true,
+        user_name: String::from("YeahItsMe"),
+        email: String::from("a@gmail.com"),
+        id: 123 
+    };
+    let user2 = User {
+        email: String::from("b@gmail.com"),
+        ..user1
+    };
+}
+
+fn build_user(email:String, user_name:String)->User{
+    let user = User{
+        email,
+        user_name,
+        active: false,
+        id: 1234
+    };
+    return  user;
 }
