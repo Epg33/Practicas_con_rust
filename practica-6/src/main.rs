443262b65f42bc3e2063507bbfe0dff60a632183
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-12-02 15:26:53 -0500

    enums

---
+++ practica-6/src/main.rs
@@ -0,0 +1,32 @@
+enum IpAddrKind {
+    V4, 
+    V6
+}
+
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String)
+}
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move {x:i32, y:i32},
+    Write(String),
+    ChangeColor(i32, i32, i32)
+}
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    let home = IpAddr::V4(128, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
+    let m = Message::Write(String::from("hello"));
+    m.call()
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:#?}", self)
+    }
+}
