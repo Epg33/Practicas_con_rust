c1756890034d9d96dd7cee6071e345546f82ddd9
EthiemGuerrero <ethiemalex03@gmail.com>
2023-04-02 21:46:14 -0500

    ea

--- course/first/src/main.rs
+++ course/first/src/main.rs
@@ -60,11 +60,66 @@ fn main() {
     //     _ => println!("Maybe")
     // }
 
-    let my_age = 18;
-    let voting_age = 18;
-    match my_age.cmp(&voting_age) {
-        Ordering::Less => println!("Cant Vote"),
-        Ordering::Greater => println!("Can Vote"),
-        Ordering::Equal => println!("Now You Cant Vote"),
+    // let my_age = 18;
+    // let voting_age = 18;
+    // match my_age.cmp(&voting_age) {
+    //     Ordering::Less => println!("Cant Vote"),
+    //     Ordering::Greater => println!("Can Vote"),
+    //     Ordering::Equal => println!("Now You Cant Vote"),
+    // }
+
+    // let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    // println!("1st: {}", arr1[0]);
+    // println!("Length: {}", arr1.len());
+    // let arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    // let mut loop_idx = 0;
+    // loop {
+    //     if arr2[loop_idx] % 2 == 0 {
+    //         loop_idx += 1;
+    //         continue;
+    //     }
+    //     if arr2[loop_idx] == 9 {
+    //         break;;
+    //     }
+    //     loop_idx += 1;
+    //     println!("Val: {}", arr2[loop_idx])
+    // }
+    // while loop_idx < arr2.len() {
+    //     println!("Arr: {}", arr2[loop_idx]);
+    // }
+
+    // for val in arr2.iter() {
+    //     println!("Arr: {}", val);
+    // }
+
+    // let my_tuple: (u8, String, f64) = (47, "Alex".to_string(), 50.000);
+    // println!("Name: {}", my_tuple.1);
+    // let (v1, v2, v3) = my_tuple;
+    // println!("Age: {}", v1)
+
+    // let mut st1 = String::new();
+    // st1.push('A');
+    // st1.push_str(" word");
+    // for word in st1.split_whitespace() {
+    //     println!("{word}");
+    // }
+    // let st2 = st1.replace("A", "another");
+    // println!("{st2}");
+
+    let st3 = String::from("x r t b h k k a m c");
+    let mut v1:Vec<char> = st3.chars().collect();
+    v1.sort();
+    v1.dedup();
+    for char in v1 {
+        println!("{char}");
     }
+
+    let st4: &str = "Random string";
+    let mut st5: String = st4.to_string();
+    println!("{st5}");
+    let byte_arr1 = st5.as_bytes();
+    let st6 = &st5[0..6];
+    println!("{st6}");
+    println!("String length: {}", st6.len());
+    st5.clear();
 }
