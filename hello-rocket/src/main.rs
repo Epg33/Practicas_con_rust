5a1298c82911129d34cf8264b2aede8e007d20eb
EthiemGuerrero <ethiemalex03@gmail.com>
2023-04-16 19:30:22 -0500

    some things

--- hello-rocket/src/main.rs
+++ hello-rocket/src/main.rs
@@ -1,5 +1,23 @@
 #[macro_use] extern crate rocket;
+use std::path::{Path, PathBuf};
+use std::io;
+use rocket::fs::FileServer;
+use rocket::fs::NamedFile;
+use rocket::tokio::task::spawn_blocking;
 use rocket::tokio::time::{sleep, Duration};
+
+#[get("/<file..>")]
+async fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).await.ok()
+}
+
+#[get("/blocking_task")]
+async fn blocking_task() -> io::Result<Vec<u8>> {
+    let vec = spawn_blocking(|| std::fs::read("data.txt")).await
+        .map_err(|e| io::Error::new(io::ErrorKind::Interrupted, e))??;
+
+    Ok(vec)
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -23,4 +41,6 @@ fn rocket() -> _ {
         .mount("/", routes![index])
         .mount("/", routes![name])
         .mount("/", routes![delay])
+        .mount("/", routes![blocking_task])
+        .mount("/", routes![files])
 }
