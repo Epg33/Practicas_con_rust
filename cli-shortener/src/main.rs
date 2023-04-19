a21e78df29345b3a4b1342d1ab0be2d33ae3f88c
EthiemGuerrero <ethiemalex03@gmail.com>
2023-04-18 21:26:26 -0500

    something

--- cli-shortener/src/main.rs
+++ cli-shortener/src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct CliArgs {
+    params: String,
+    command: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = CliArgs::parse();
+    match args.params {
+        "new" => println!("New command"),
+        "insert" => println!("Insert command"),
+        "erase" => println!("Erase command"),
+        "update" => println!("Update command"),
+    }
 }
