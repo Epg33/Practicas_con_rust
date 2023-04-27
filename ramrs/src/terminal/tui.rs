907dd00de3ea17339b66385ef2ed4f02e5af9c41
EthiemGuerrero <ethiemalex03@gmail.com>
2023-04-27 14:03:27 -0500

    make the ui of the terminal

--- ramrs/src/terminal/tui.rs
+++ ramrs/src/terminal/tui.rs
@@ -1,10 +1,10 @@
 pub mod tui {
   use std::{io, thread, time::Duration};
   use tui::{
-    backend::CrosstermBackend,
+    backend::{CrosstermBackend, Backend},
     widgets::{Widget, Block, Borders},
     layout::{Layout, Constraint, Direction}, 
-    Terminal}; 
+    Terminal, Frame}; 
   use crossterm::{
     event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
     execute,
@@ -19,11 +19,12 @@ pub mod tui {
     let mut terminal = Terminal::new(backend)?;
 
     terminal.draw(|f| {
-      let size = f.size();
-      let block = Block::default()
-        .title("Block")
-        .borders(Borders::ALL);
-      f.render_widget(block, size);
+      ui(f);
+      // let size = f.size();
+      // let block = Block::default()
+      //   .title("Block")
+      //   .borders(Borders::ALL);
+      // f.render_widget(block, size);
     })?;
 
     thread::sleep(Duration::from_millis(5000));
@@ -33,5 +34,21 @@ pub mod tui {
     terminal.show_cursor()?;
 
     Ok(())
+  }
+
+  fn ui<B: Backend>(f: &mut Frame<B>) {
+    let chuncks = Layout::default()
+      .direction(Direction::Vertical)
+      .margin(1)
+      .constraints([
+        Constraint::Percentage(10),
+        Constraint::Percentage(80),
+        Constraint::Percentage(10)
+      ].as_ref())
+      .split(f.size());
+    let block = Block::default()
+        .title("Block")
+        .borders(Borders::ALL);
+      f.render_widget(block, chuncks[1]);
   }
 }
